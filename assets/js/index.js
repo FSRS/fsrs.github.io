a332c769a2d115f13e71f8bdb7c5c48720a91391
FSRS <58435406+FSRS@users.noreply.github.com>
2020-07-23 17:44:17 +0900

    renewal2

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+const collapse = document.querySelector(".collapse-container-header");
+
+collapse.addEventListener("click", () => {
+  const content = document.querySelector(".collapse-container-content");
+  if (content.style.display === "none") content.style.display = "";
+  else content.style.display = "none";
+});
